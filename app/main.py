47238e4e134cd2ccfdd0a45e090e531eb621de99
Juan Jose <juanobando.developer@gmail.com>
2022-11-18 14:43:27 -0500

    conexion satisfactoria con MONGODB

--- app/main.py
+++ app/main.py
@@ -1,61 +1,141 @@
-from pydantic import BaseModel
-from fastapi import FastAPI
+import pymongo # solo para la conexion a la base de datos
+from pydantic import BaseModel, Field # para crear modelos para cada entidad (usuario / documento) y sus campos
+from fastapi import FastAPI # para crear la API
+from bson import ObjectId # para convertir el id de mongo a string o viceversa
+from typing import Optional, List # para usar en el metodo put o update de la API y poder actualizar solo algunos campos además de usar List para devolver una lista de documentos o usuarios
 
-app = FastAPI(title = "Gestión documental", description = "API para la gestión documental de la empresa", version = "1.0.0")
+app = FastAPI(title = "Gestión documental", description = "API para la gestión documental de la empresa", version = "1.0.0") # creamos la API
+client = pymongo.MongoClient() #Link de base de datos dentro de los parentesis del metodo MongoClient de pymongo
+db = client.databaseName # reemplazar databaseName por el nombre de la base de datos en mongo
 
-Documents = [
-    {
-        "id": 1,
-        "name": "Document 1",
-        "description": "This is the first document"
-    },
-    {
-        "id": 2,
-        "name": "Document 2",
-        "description": "This is the second document"
-    },
-    {
-        "id": 3,
-        "name": "Document 3",
-        "description": "This is the third document"
-    },
-    {
-        "id": 4,
-        "name": "Document 4",
-        "description": "This is the fourth document"
-    },
-]
+class PyObjectId(ObjectId):
+    # PyObjectId es una clase que hereda de ObjectId de bson para poder convertir el id de mongo a string y viceversa
+    # es indispensable para poder usar el id de mongo como string en la API
+    # debe ir en cada api que se vaya a crear con mongo
+    #Cada @classmethod es un metodo indispensable y obligatorio de usar de la forma como se expresa en el codigo
+    # Status: OBLIGATORIA
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
 
-Users = [
-    {
-        "id": 1,
-        "name": "User 1",
-        "description": "This is the first user"
-    },
-    {
-        "id": 2,
-        "name": "User 2",
-        "description": "This is the second user"
-    },
-    {
-        "id": 3,
-        "name": "User 3",
-        "description": "This is the third user"
-    },
-    {
-        "id": 4,
-        "name": "User 4",
-        "description": "This is the fourth user"
-    }
-]
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError('Invalid object id')
+        return ObjectId(v)
 
-@app.get("/")
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type='string')    
+
+class DocumentoModel(BaseModel):
+    # modelo para crear un documento
+    # Status: OBLIGATORIA
+    # se debe generar de la siguiente forma:
+    # class NombreModel(BaseModel):
+    #     campo1: tipoCampo1 = Field(...) # Field es para agregar una descripcion al campo
+    #     campo2: tipoCampo2 = Field(...)
+
+    #     class Config:
+    #         # Config es para agregar una descripcion al modelo
+    #         # la descripcion es opcional
+
+    #         title = "Nombre del modelo"
+    #         description = "Descripcion del modelo"
+    #         version = "1.0.0"
+
+    #         # json_encoders es obligatorio para poder usar el id de mongo como string en la API
+    #         json_encoders = {ObjectId: str} # para convertir el id de mongo a string
+
+    #         # schema_extra es para agregar un ejemplo de como debe ser el modelo
+    #         # es opcional
+    #         schema_extra = {
+    #             "example": {
+    #                 "campo1": "valorCampo1",
+    #                 "campo2": "valorCampo2"
+    #             }
+    #         }
+
+    # el campo id es opcional ya que mongo lo genera automaticamente
+    # en caso de usar el campo id, debe ser de tipo PyObjectId
+
+    # ejemplo de un modelo:
+
+    id: PyObjectId = Field(default_factory=PyObjectId, alias='_id')
+    nombre: str = Field(...)
+    descripcion: str = Field(...)
+
+    class Config:
+        json_encoders = { ObjectId: str }
+
+class UpdateDocumentoModel(BaseModel):
+    # modelo para actualizar un documento
+    # Status: OBLIGATORIA
+    # se debe generar de la siguiente forma:
+    # class NombreModel(BaseModel):
+    #     campo1: Optional[tipoCampo1] = Field(...) # Field es para agregar una descripcion al campo
+    #     campo2: Optional[tipoCampo2] = Field(...) # Optional es para que el campo sea opcional
+
+    #     class Config:
+    #         # Config es para agregar una descripcion al modelo
+    #         # la descripcion es opcional
+
+    #         title = "Nombre del modelo"
+    #         description = "Descripcion del modelo"
+    #         version = "1.0.0"
+    
+    #         # json_encoders es obligatorio para poder usar el id de mongo como string en la API
+    #         json_encoders = {ObjectId: str} # para convertir el id de mongo a string
+
+    #         # schema_extra es para agregar un ejemplo de como debe ser el modelo
+    #         # es opcional
+    #         schema_extra = {
+    #             "example": {
+    #                 "campo1": "valorCampo1",
+    #                 "campo2": "valorCampo2"
+    #             }
+    #         }
+
+    # el campo id no es necesario ya que no se debe actualizar
+
+    # ejemplo de un modelo para actualizar:
+    nombre: Optional[str]
+    descripcion: Optional[str]
+
+    class Config:
+        json_encoders = { ObjectId: str }
+
+class UserModel(BaseModel):
+    # repetir la misma sintaxis de documento model pero para usuarios o distintas entidades
+    id: PyObjectId = Field(default_factory=PyObjectId, alias='_id')
+    Nombre: str = Field(...)
+    Apellidos: str = Field(...)
+    Edad: int = Field(...)
+    Curso: str = Field(...)
+
+    class Config:
+        json_encoders = { ObjectId: str }
+
+class UpdateUserModel(BaseModel):    
+    # repetir la misma sintaxis de update documento model pero para usuarios o distintas entidades
+    Nombre: Optional[str]
+    Apellidos: Optional[str]
+    Edad: Optional[int]
+    Curso: Optional[str]
+
+    class Config:
+        json_encoders = { ObjectId: str }
+
+# Rutas de la API
+# Ruta principal:
+@app.get("/", response_description="API saying hello") # response description permite agregar una descripcion a la respuesta de la API en swagger
 async def read_root():
     return {"Hello": "World"}
 
-@app.get("/user/")
-async def getUsers(skip: int = 0, limit: int = 10):
-    return Users[skip: skip + limit]
+# Ruta para listar todos los usuarios
+@app.get("/user/", response_model=List[UserModel]) # response model permite agregar un modelo a la respuesta de la API en swagger, OBLIGATORIO
+async def getUsers():
+    return list(db["Tripulantes"].find()) # db es la base de datos, "Tripulantes" es la coleccion, find() es para buscar todos los documentos de la coleccion y list() es para convertir el resultado de find() a una lista
 
 @app.get("/user/{user_id}")
 async def getUser(user_id: int):
